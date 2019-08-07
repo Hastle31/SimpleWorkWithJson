adde661bc1e86a93630eea0808ebd81188eb3384
vladislav.ostapov <vladislav.ostapov@r-express.ru>
2019-08-07 12:56:21 +0300

    Тест отсутствия зависимости библиотеки Domain от DataAccess

---
+++ xTests/DllReferenceTest.cs
@@ -0,0 +1,25 @@
+﻿using SimpleWorkWithJson.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace xTests
+{
+    public class DllReferenceTest
+    {
+        /// <summary>
+        /// Библиотека Domain не должна зависеть от DataAccess.
+        /// </summary>
+        [Fact]
+        public void DomainLibraryIndepended()
+        {
+            var type = typeof(IHostInfoService);
+            var referencesAssembly = type.Assembly
+                .GetReferencedAssemblies()
+                .Where(x => x.Name.StartsWith("SimpleWorkWithJson"));
+            Assert.Empty(referencesAssembly);
+        }
+    }
+}
